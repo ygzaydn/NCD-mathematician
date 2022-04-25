19b76276e5bff4c7c0c2d22ae4939853516bc679
ygzaydn <ygzaydns@gmail.com>
2022-04-25 16:30:37 +0300

    scripts readme

---
+++ scripts/README.md
@@ -0,0 +1,6 @@
+# Scripts
+
+This project has its own frontend, which means that you have to obtain score to get points, but for a case I've simulated scripts that user has 100 points. On this directory, you can 2 script files. 
+
+	-	`deploy.sh` deploys contract to your own account if you want to.
+	-	`contract.sh` simulates the game as you've get 100points, so you've earned 1NEAR as gift.
--- scripts/contract.sh
+++ scripts/contract.sh
@@ -1,12 +1,17 @@
+echo "Enter owner name that has contract:"
+read OWNER_
+
+export OWNER=$OWNER_
+
 echo "Enter contract name to play game:"
 read CONTRACT_
 
 export CONTRACT=$CONTRACT_
 
 echo "Entering game with 1 NEAR..."
-near call nerdkrypto.testnet getTicket --account-id $CONTRACT
+near call $OWNER getTicket --account-id $CONTRACT
 
 
 echo "Game ended and your score is 100, so you earn 1NEAR"
-near call nerdkrypto.testnet finishGame '{"amount":"10000000000000000000000"}' --account-id $CONTRACT
+near call $OWNER finishGame '{"amount":"10000000000000000000000"}' --account-id $CONTRACT
 
